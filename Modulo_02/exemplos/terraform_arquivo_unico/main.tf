989f59713f575729db4aa9a07eab80d3f6c4da37
joaomarceloalencar <joao.marcelo@ufc.br>
2024-02-22 12:45:48 -0300

    Exemplo de arquivo único do Terraform.

---
+++ Modulo_02/exemplos/terraform_arquivo_unico/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "devops" {
+  name        = "devops"
+  description = "Grupo de Seguranda da Disciplina DevOps"
+
+  tags = {
+    Name = "devops"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "permitir_ssh_ipv4" {
+  security_group_id = aws_security_group.devops.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-0c7217cdde317cfec"
+  instance_type   = "t2.micro"
+  key_name        = "vockey"
+  security_groups = ["devops"]
+
+  tags = {
+    Name = "InstanciaDevOps"
+  }
+}
+
