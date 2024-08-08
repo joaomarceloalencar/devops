eb9653143895dae2b9ea69f817ebeec64130062f
joaomarceloalencar <joao.marcelo@ufc.br>
2024-08-08 08:30:11 -0300

    Alterando a localização dos templates.

--- Laboratorios/01_Flask/src/app.py
+++ Laboratorios/01_Flask/src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for 
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='../templates')
 
 # /// = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
