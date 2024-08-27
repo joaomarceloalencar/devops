fb1a01212599d430302e8f4fcbf4db11232849de
jmhal <joao.marcelo@ufc.br>
2024-08-27 19:27:22 -0300

    Corrigindo a identação.

--- Modulo_02/instalar_terraform.sh
+++ Modulo_02/instalar_terraform.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 # Este script instala o Terraform no Ubuntu.
 # Comandos retirados de https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+# Execute com sudo.
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+apt-get update && sudo apt-get install -y gnupg software-properties-common
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
-
-gpg --dearmor | \
-
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    gpg --dearmor | \
+    tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
 
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo tee /etc/apt/sources.list.d/hashicorp.list
+apt update
 
-sudo apt update
-
-sudo apt-get install terraform
+apt-get install terraform
