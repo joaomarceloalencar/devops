9c50e6dfb3119e64e025e10a1f1f566a49e1fe6c
jmhal <joao.marcelo@ufc.br>
2024-08-27 19:44:48 -0300

    Corrigindo o nome do grupo de segurança.

--- Modulo_02/exemplos/terraform/arquivo_unico/main.tf
+++ Modulo_02/exemplos/terraform/arquivo_unico/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "app_server" {
   ami             = "ami-0c7217cdde317cfec"
   instance_type   = "t2.micro"
   key_name        = "vockey"
-  security_groups = ["devops"]
+  security_groups = [ aws_security_group.devops.name ]
 
   tags = {
     Name = "InstanciaDevOps"
