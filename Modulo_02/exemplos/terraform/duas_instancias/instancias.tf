ab6991e28c675933f64987c66c84a0f07f84e0e3
joaomarceloalencar <joao.marcelo@ufc.br>
2024-08-30 15:05:38 -0300

    Mais um ajuste nos grupos de seguranças.

--- Modulo_02/exemplos/terraform/duas_instancias/instancias.tf
+++ Modulo_02/exemplos/terraform/duas_instancias/instancias.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "servidorweb" {
   ami             = "ami-0c7217cdde317cfec"
   instance_type   = "t2.micro"
   key_name        = "vockey"
-  security_groups = ["web"]
+  security_groups = [ aws_security_group.web.nome ]
 
   tags = {
     Name = var.nome_instancia_web
@@ -13,7 +13,7 @@ resource "aws_instance" "servidorbancodedados" {
   ami             = "ami-0c7217cdde317cfec"
   instance_type   = "t2.micro"
   key_name        = "vockey"
-  security_groups = ["bancodedados"]
+  security_groups = [ aws_security_group.bancodedados.nome ]
 
   tags = {
     Name = var.nome_instancia_bd
--- Modulo_02/exemplos/terraform/varios_arquivos/instancia.tf
+++ Modulo_02/exemplos/terraform/varios_arquivos/instancia.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "servidor_devops" {
   ami             = "ami-0c7217cdde317cfec"
   instance_type   = "t2.micro"
   key_name        = "vockey"
-  security_groups = ["devops"]
+  security_groups = [ aws_security_group.devops.name ]
 
   tags = {
     Name = var.nome_instancia
