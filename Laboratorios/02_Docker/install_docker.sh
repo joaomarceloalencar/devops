ff5675ec8311dda2d2ad24bd9363d2c615b7229f
joaomarceloalencar <joao.marcelo@ufc.br>
2024-09-03 20:17:59 -0300

    Esqueci uma confirmação.

--- Laboratorios/02_Docker/install_docker.sh
+++ Laboratorios/02_Docker/install_docker.sh
@@ -8,7 +8,7 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 
 # Add Docker's official GPG key:
 apt-get update
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
