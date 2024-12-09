0a0e4f7e50f7e12960cdfa0b63e1e0cadcf9ad2f
joaomarceloalencar <joao.marcelo@ufc.br>
2024-12-09 15:47:19 -0300

    Liberando mais portas para o Kubernetes.

--- Modulo_05/terraform/grupo_de_seguranca.tf
+++ Modulo_05/terraform/grupo_de_seguranca.tf
@@ -24,6 +24,22 @@ resource "aws_vpc_security_group_ingress_rule" "permitir_ssh_ipv4" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "permitir_http_ipv4" {
+  security_group_id = aws_security_group.kubernetes.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "permitir_nodeport_ipv4" {
+  security_group_id = aws_security_group.kubernetes.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 30000
+  ip_protocol       = "tcp"
+  to_port           = 32767 
+}
+
 resource "aws_vpc_security_group_egress_rule" "trafego_saida" {
   security_group_id = aws_security_group.kubernetes.id
   from_port        = -1
